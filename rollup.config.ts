d584fe588b78b29858ed628bb2e9fd6741ed2895
Trim21 <trim21.me@gmail.com>
2023-05-17 10:36:19 +0800

    build: merge tsconfig

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,5 +1,5 @@
-import type { RollupOptions } from 'rollup';
 import typescript from '@rollup/plugin-typescript';
+import type { RollupOptions } from 'rollup';
 
 export default {
   input: 'src/index.ts',
@@ -8,5 +8,13 @@ export default {
     format: 'esm',
     sourcemap: true,
   },
-  plugins: [typescript({ tsconfig: 'tsconfig.build.json' })],
+  plugins: [
+    typescript({
+      tsconfig: 'tsconfig.json',
+      compilerOptions: {
+        rootDir: 'src',
+      },
+      exclude: ['**/node_modules/**', '**/__test__/**', 'dist'],
+    }),
+  ],
 } satisfies RollupOptions;
