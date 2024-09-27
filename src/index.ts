6fd26f09d2e3b2d2de4fb2b48b6dad01a4397a81
Trim21 <trim21.me@gmail.com>
2024-09-27 14:20:10 +0800

    add missing exports

--- src/index.ts
+++ src/index.ts
@@ -13,7 +13,7 @@ import { WikiArrayItem, WikiItem } from './types.js';
 
 export * from './types.js';
 export * from './error.js';
-export { stringify } from './stringify.js';
+export { stringify, stringifyMap } from './stringify.js';
 
 /** 解析 wiki 文本，以 `Map` 类型返回解析结果。 会合并重复出现的 key */
 export function parseToMap(s: string): WikiMap {
